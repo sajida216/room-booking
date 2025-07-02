b0673b70ef8bd61c439364abb7760f0d1f60f049
sajida216 <ayishathsajidha@gmail.com>
2025-07-02 22:41:39 +0530

    change router2

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -9,8 +9,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
-  <Router basename="/room-booking">
-    <App />
-  </Router>
+    <BrowserRouter basename="/room-booking">
+      <App />
+    </BrowserRouter>
 </StrictMode>
 )
