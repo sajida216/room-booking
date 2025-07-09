6a8fb223b3b60ce8096e4e8f8bdfa7a21dbd65df
sajida216 <ayishathsajidha@gmail.com>
2025-07-09 12:59:05 +0530

    change para

--- src/compenents/expirience/Expirience.jsx
+++ src/compenents/expirience/Expirience.jsx
@@ -21,7 +21,7 @@ const Expirience = () => {
     <div className='position-absolute ' style={{ top: "10%", left: "40%", zIndex: 1}}>
          <p className="fs-4">The Convenient Hotel</p>
          <p className="fs-3">Discover unforgettable experiences with every stay at our Hotels.</p>
-         <p style={{fontSize:"10px"}}>At StayFinder, we believe every stay should be more than just a place to rest. Our carefully curated hotels offer a perfect blend of comfort, elegance, and personalized service to make your trip truly unforgettable. Whether you’re traveling for business or leisure, our accommodations are designed to create memories that last a lifetime. Indulge in world-class amenities, savor delicious cuisines, and enjoy unmatched hospitality. With StayFinder, your journey becomes as delightful as the destination</p>
+         <p style={{fontSize:"10px"}}>At StayFinder, we believe every stay should be more than just a place to rest. Our carefully curated hotels offer a perfect blend of comfort.</p>
     </div>
    
   </div>
